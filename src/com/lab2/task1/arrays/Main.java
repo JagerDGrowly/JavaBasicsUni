b1c0e80458e84fc19e14baccc935f0387ff7a386
JagerDGrowly <vovkapedenko@gmail.com>
2025-04-01 14:54:16 +0200

    Task 1 using java.util.Arrays

--- src/com/lab2/task1/arrays/Main.java
+++ src/com/lab2/task1/arrays/Main.java
@@ -2,22 +2,39 @@ package com.lab2.task1.arrays;
 
 import java.util.*;
 
-public class TwoArrayDemo {
+public class Main {
     public static void main(String[] args) {
         int m = 4;
         int n = 6;
-
         int from = 4;
         int to = 33;
 
         int[][] arr = new int[m][];
+        int[] maxes = new int[m];
+        String[] linesArr = new String[m];
         Random r = new Random();
+
+        System.out.print("Enter a char: ");
+        String symbol = new Scanner(System.in).nextLine();
+
+        makeArray(arr, r, n, from, to);
+        makeArray(arr, maxes);
+        makeArray(maxes, linesArr, symbol);
+    }
+
+    public static void makeArray(int[][] arr, Random r, int n, int from, int to) {
         Arrays.setAll(arr, i -> fillRow(r, n, from, to));
         System.out.println(Arrays.deepToString(arr));
+    }
 
-        int[] maxes = new int[m];
+    public static void makeArray(int[][] arr, int[] maxes) {
         Arrays.setAll(maxes, i -> getMaxInRow(arr, i));
         System.out.println(Arrays.toString(maxes));
+    }
+
+    public static void makeArray(int[] maxes, String[] linesArr, String symbol) {
+        Arrays.setAll(linesArr, i -> symbol.repeat(maxes[i]));
+        Arrays.stream(linesArr).forEach(System.out::println);
     }
 
     public static int[] fillRow(Random random, int size, int from, int to) {
