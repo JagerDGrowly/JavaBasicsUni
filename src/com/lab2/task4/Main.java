c5ecc4136ac873a20bac82c295af469e137721ae
JagerDGrowly <vovkapedenko@gmail.com>
2025-04-05 11:02:43 +0200

    added javadoc comments

--- src/com/lab2/task4/Main.java
+++ src/com/lab2/task4/Main.java
@@ -2,7 +2,20 @@ package com.lab2.task4;
 
 import java.util.*;
 
+/**
+ * A class that evenly distributes spaces between words in a given string to match a specified length.
+ * The program takes a string as a command-line argument and a desired length as user input, then
+ * adjusts the spaces between words to achieve the target length while preserving the words themselves.
+ */
 public class Main {
+    /**
+     * The main method that processes a string from command-line arguments, adjusts the spacing
+     * between words to match a user-specified length, and prints the resulting string.
+     * The number of spaces is distributed as evenly as possible between words, with any remaining
+     * spaces added to the first few gaps.
+     *
+     * @param args command-line arguments, where args[0] is the input string containing words separated by spaces
+     */
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
 
