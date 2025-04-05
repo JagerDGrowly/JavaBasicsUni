cdc5fa6d48340538d5911aa75b17dd9dc1d0544d
JagerDGrowly <vovkapedenko@gmail.com>
2025-04-05 13:04:07 +0200

    started creating second entity

---
+++ src/com/lab2/task5/Kiosk.java
@@ -0,0 +1,58 @@
+package com.lab2.task5;
+
+import java.util.Arrays;
+
+public class Kiosk {
+    private String name;
+    private String address;
+    private Hour[] hours;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Hour getHour(int i) {
+        return hours[i];
+    }
+
+    public void setHour(int i, Hour hour) {
+        hours[i] = hour;
+    }
+
+    public void addHour(Hour hour) {
+        setHours(HourUtilities.addToArray(getHours(), hour));
+    }
+
+    public void addHour(int numberOfCustomers, String comments) {
+        Hour hour = new Hour(numberOfCustomers, comments);
+        addHour(hour);
+    }
+
+    public int hoursCount() {
+        return hours.length;
+    }
+
+    public void clearHours() {
+        hours = null;
+    }
+
+    public Hour[] getHours() {
+        return hours;
+    }
+
+    public void setHours(Hour[] hours) {
+        this.hours = hours;
+    }
+}
---
+++ src/com/lab2/task5/KioskUtilities.java
@@ -0,0 +1,7 @@
+package com.lab2.task5;
+
+import static com.lab2.task5.HourUtilities.*;
+
+public class KioskUtilities {
+
+}
