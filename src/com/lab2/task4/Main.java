f21e8e3da21a410c75366e321ac9edf78d426e79
JagerDGrowly <vovkapedenko@gmail.com>
2025-04-05 10:48:50 +0200

    rewrote task. fixed problem with splitting the array. now the program fills an equal number of spaces between words in the argument

--- src/com/lab2/task4/Main.java
+++ src/com/lab2/task4/Main.java
@@ -5,24 +5,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        int interval = n - args[0].length();
+
         String[] line = args[0].split(" ");
+
+        int totalWordLength = 0;
+        for (String word : line) {
+            totalWordLength += word.length();
+        }
+
+        int interval = n - totalWordLength;
         int spaces = line.length - 1;
-        int iterations = interval / spaces;
-        int end = interval % spaces;
-
-        for (int i = 0; i < line.length; i++) {
-            line[i] = line[i].concat(" ").repeat(iterations);
-        }
-
-        for (int i = 0; i < end; i++) {
-            line[i] = line[i].concat(" ");
-        }
+        int iterations = spaces > 0 ? interval / spaces : 0;
+        int end = spaces > 0 ? interval % spaces : 0;
 
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < line.length - 1; i++) {
+        for (int i = 0; i < line.length; i++) {
             result.append(line[i]);
+            if (i < line.length - 1) {
+                result.append(" ".repeat(iterations));
+                if (i < end) {
+                    result.append(" ");
+                }
+            }
         }
+
         System.out.println(result);
     }
 }
